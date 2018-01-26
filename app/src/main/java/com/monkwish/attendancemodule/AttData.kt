b0b89ba0d3dc287cbd6ed40dabbd4c9c9e6404d8
Viplove <viplovemittal2@gmail.com>
2018-01-26 15:09:26 +0530

    Import done

---
+++ app/src/main/java/com/monkwish/attendancemodule/AttData.kt
@@ -0,0 +1,6 @@
+package com.monkwish.attendancemodule
+
+/**
+ * Created by viplo on 26-Jan-18.
+ */
+data class AttData(val slot : String,val sunName : String,val subCode : String,val FacName : String,val roomno : Int,val presentNo : Int,val totalCNo : Int)
